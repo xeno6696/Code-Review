7ed53ee29d8fa37d35ecf6d3d80d00611c693692
Matt Seil <xeno6696@gmail.com>
2023-11-11 14:15:43 -0700

    Added example of an integer overflow error.

---
+++ Java/ExampleErrors/Overflow.java
@@ -0,0 +1,11 @@
+package ExampleErrors;
+
+public class Overflow {
+    public static void main(String[] args) {
+        int value = Integer.MAX_VALUE;
+
+        System.out.println(value);
+        System.out.println(value + 1);
+        System.exit(0);
+    }
+}
