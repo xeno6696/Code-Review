1036e8d4c7474b48c593d11aca5e723fe2ad6558
Matt Seil <xeno6696@gmail.com>
2023-11-13 16:32:36 -0700

    Added first exercise.

--- Java/functions/FuncUtils.java
+++ Java/functions/FuncUtils.java
@@ -1,5 +1,14 @@
 package functions;
 
+/**
+ * XUtils classes are an old but still oft-used idea where you create a class that simply collects a large array 
+ * of useful and related functions.  Apache has StringUtils for example that even provides some nominal escaping
+ * libraries for output escaping.  However, in java programming land, classes like this are often viewed as "bad"
+ * as they essentially break the idea that Java is supposed to be an OOP language.  
+ * 
+ * We won't cover that in this class really because OOP design has had more pages written about it than exist in the
+ * Bible + Qu'ran combined.  
+ */
 public class FuncUtils {
     public static void main(String args[]){
         short foo = 1;
