7ea32e0854d0ace3b1333970ba8dc302c809118a
Matt Seil <xeno6696@gmail.com>
2023-12-14 08:15:38 -0700

    Added a question on HelloArgs.java

--- Java/HelloArgs.java
+++ Java/HelloArgs.java
@@ -1,6 +1,7 @@
 public class HelloArgs {
 
     public static void main(String [] args){
+        //Wait!  if Java is based on C... where is argc?
         for(String arg : args){
             System.out.println(arg);
         }
