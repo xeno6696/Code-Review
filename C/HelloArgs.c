63f2c76bbf04fd78913f42b443c2d57561429441
Matt Seil <xeno6696@gmail.com>
2024-01-19 09:01:39 -0700

    Added dave's awesome block comment toggle trick.

--- C/HelloArgs.c
+++ C/HelloArgs.c
@@ -7,7 +7,8 @@
 int main(int argc, char *argv[]){
 
     char * output = NULL;
-    
+    //char * output;  
+    //*
     for(int i = 0; i < argc; i++){
         int argLength = strlen(argv[i]);                         //compute length of the argument
         output = realloc(output, argLength * sizeof(char) + 1);  //Allocate an array the size of the argument plus a null byte
@@ -16,6 +17,7 @@ int main(int argc, char *argv[]){
         printf("%s\n", output);                                  //print the string
                                                     
     }
+    //*/
     //printf("%s\n", "Freeing Memory!");
     free(output);                                                //always undo what was done (i.e. memory, closing files, etc.)  
     return EXIT_SUCCESS; //Or zero!
